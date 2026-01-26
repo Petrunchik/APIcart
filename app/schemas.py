2895ea81df40afbbd11da356c18966ad2ed7224b
Petr <petrlinkov1376@gmail.com>
2026-01-26 15:30:01 +0300

    schemas: Доработаны модели pydantic для валидации запросов и ответов

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, Field, ConfigDict
+from decimal import Decimal
 
 class CategoryCreate(BaseModel):
     """
@@ -18,5 +19,31 @@ class CategoryAnswer(CategoryCreate):
     """
     id: int = Field(..., description="ID категории")
     is_active: bool = Field(..., description="Активность категории")
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class ProductCreate(BaseModel):
+    """
+    Модель для создания и обновления товара. 
+
+    POST и PUT запросы. 
+    """
+    name: str = Field(..., min_length=3, max_length=100, description="Название категории (3-100 символов).")
+    description: str | None = Field(None, max_length=500, description="Описание товара, не более 500 символов.")
+    price: Decimal = Field(..., gt=0, decimal_places=2, description="Цена товара, больше 0.")
+    image_url: str | None = Field(None, max_length=200, description="Ссылка на изображение товара.")
+    stock: int = Field(..., ge=0, description="Остаток на складе, не меньше 0.")
+    category_id: int = Field(..., description="ID категориии, к которой относится товар.")
+
+
+class ProductAnswer(ProductCreate):
+    """
+    Модель для ответа с данными товара.
+
+    GET запросы.
+    """
+    id: int = Field(..., description="ID товара.")
+    is_active: bool = Field(..., description="Активность товара.")
 
     model_config = ConfigDict(from_attributes=True)
