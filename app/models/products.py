f6484d8f85b32642835783fc04b7a2c77916942c
Petr <petrlinkov1376@gmail.com>
2026-01-27 12:35:34 +0300

    app/models: Добавлены файлы с моделями таблиц для БД

---
+++ app/models/products.py
@@ -0,0 +1,21 @@
+from decimal import Decimal
+from sqlalchemy import String, Boolean, Numeric, Integer, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.database import Base
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    description: Mapped[str] = mapped_column(String(500), nullable=False)
+    price: Mapped[Decimal] = mapped_column(Numeric(10, 2), nullable=False)
+    image_url: Mapped[str | None] = mapped_column(String(200))
+    stock: Mapped[int] = mapped_column(Integer)
+    is_active: Mapped[bool] = mapped_column(Boolean)
+    category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"), nullable=False)
+    category: Mapped["Category"] = relationship(
+        "Category",
+        back_populates="product"
+    )
