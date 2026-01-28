0e1dcbfffa3582d6885d94daa4cceb14a1988ec7
Petr <petrlinkov1376@gmail.com>
2026-01-28 17:59:50 +0300

    app/db_depends.py: Исправление ошибок

--- app/db_depends.py
+++ app/db_depends.py
@@ -25,5 +25,5 @@ async def get_async_db() -> AsyncGenerator[AsyncSession, None]:
     """
     Предоставляет асинхронную сессию SQLAlchemy для работы с базой данных PostgreSQL.
     """
-    async with async_session_maker as session:
+    async with async_session_maker() as session:
         yield session
