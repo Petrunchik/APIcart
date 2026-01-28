a73497c08c758f225d240846919ee17fdbf4be47
Petr <petrlinkov1376@gmail.com>
2026-01-28 12:20:13 +0300

    app/models/categories.py: Исправлена ошибка в поле is_active

--- app/models/categories.py
+++ app/models/categories.py
@@ -9,9 +9,9 @@ class Category(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(50), nullable=False)
     parent_id: Mapped[int | None] = mapped_column(ForeignKey("categories.id"), nullable=True)
-    is_active: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
 
-    products: Mapped[list["Product"]] = relationship(
+    product: Mapped[list["Product"]] = relationship(
         "Product",
         back_populates="category"
     )
