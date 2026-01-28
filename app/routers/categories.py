312c6c987015d085c9fa79cf83d3ae7a70965922
Petr <petrlinkov1376@gmail.com>
2026-01-28 18:50:02 +0300

    app/routers/categories.py: Переход на асинхронные запросы

--- app/routers/categories.py
+++ app/routers/categories.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy import select, update
 from sqlalchemy.orm import Session
-from app.db_depends import get_db
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.db_depends import get_db, get_async_db
 from app.schemas import CategoryAnswer, CategoryCreate
 from app.models.categories import Category as CategoryModel
 
@@ -12,73 +13,77 @@ router = APIRouter(
 
 
 @router.get("/", response_model=list[CategoryAnswer], status_code=status.HTTP_200_OK)
-async def get_all_categories(db: Session = Depends(get_db)):
+async def get_all_categories(db: AsyncSession = Depends(get_async_db)):
     """
     Возвращает список всех категорий товаров
     """
-    categories = db.scalars(select(CategoryModel).where(CategoryModel.is_active == True)).all()
-    return categories
+    categories = await db.scalars(select(CategoryModel).where(CategoryModel.is_active == True))
+    return categories.all()
 
 
 @router.post("/", response_model=CategoryAnswer, status_code=status.HTTP_201_CREATED)
-async def create_new_category(category: CategoryCreate, db: Session = Depends(get_db)):
+async def create_new_category(category: CategoryCreate, db: AsyncSession = Depends(get_async_db)):
     """
     Добавляет новую категорию
     """
     if category.parent_id is not None:
-        parent = db.scalars(
+        stmt = await db.scalars(
             select(CategoryModel).where(
                 CategoryModel.id == category.parent_id,
                 CategoryModel.is_active == True)
-                ).first()
+                )
+        parent = stmt.first()
         if parent is None:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Parent category not found!")
         
     db_category = CategoryModel(**category.model_dump())
     db.add(db_category)
-    db.commit()
-    db.refresh(db_category)
+    await db.commit()
+    await db.refresh(db_category)
     return db_category
 
 
 @router.put("/{category_id}", response_model=CategoryAnswer, status_code=status.HTTP_200_OK)
-async def update_category(category_id: int, updated_category: CategoryCreate, db: Session = Depends(get_db)):
+async def update_category(category_id: int, updated_category: CategoryCreate, db: AsyncSession = Depends(get_async_db)):
     """
     Изменяет категорию по ее ID
     """
-    category = db.scalars(select(CategoryModel).where(
+    stmt = await db.scalars(select(CategoryModel).where(
         CategoryModel.id == category_id,
         CategoryModel.is_active == True
-        )).first()
+        ))
+    category = stmt.first()
     if category is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Category not found!")
     if category.parent_id is not None:
-        parent = db.scalars(select(CategoryModel).where(
+        parent_stmt = await db.scalars(select(CategoryModel).where(
             CategoryModel.id == category.parent_id,
             CategoryModel.is_active == True
-        )).first()
+        ))
+        parent = parent_stmt.first()
         if parent is None:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Parent category not found")
     
-    db.execute(update(CategoryModel).where(
+    await db.execute(update(CategoryModel).where(
         CategoryModel.id == category_id
     ).values(**updated_category.model_dump()))
-    db.commit()
-    db.refresh(category)
+    await db.commit()
+    await db.refresh(category)
     return category
 
 
 @router.delete("/{category_id}", status_code=status.HTTP_200_OK)
-async def delete_category(category_id: int, db: Session = Depends(get_db)):
+async def delete_category(category_id: int, db: AsyncSession = Depends(get_async_db)):
     """
     Удаляет категорию по ID
     """
-    category = db.scalars(select(CategoryModel).where(
+    stmt = await db.scalars(select(CategoryModel).where(
         CategoryModel.id == category_id,
         CategoryModel.is_active == True
-    )).first()
+    ))
+    category = stmt.first()
     if category is None:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Category not found!")
     category.is_active = False
-    db.commit()
+    await db.commit()
     return {"message": "Category is marked as deleted"}
