010bfa407bd4dd93279b4d00778faaeea8f3c57a
Petr <petrlinkov1376@gmail.com>
2026-01-28 19:15:14 +0300

    app/db_depends.py: Удалена синхронная сессия тк проект переехал на асинхронную

--- app/db_depends.py
+++ app/db_depends.py
@@ -1,21 +1,3 @@
-from sqlalchemy.orm import Session
-from collections.abc import Generator
-
-from app.database import SessionLocal
-
-
-def get_db() -> Generator[Session, None, None]:
-    """
-    Зависимость для получения сессии базы данных. 
-
-    Создает новую сессию для каждого запроса и закрывает после обработки. 
-    """
-    db: Session = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
 from sqlalchemy.ext.asyncio import AsyncSession
 from collections.abc import AsyncGenerator
 
