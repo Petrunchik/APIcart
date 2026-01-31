706ae58e3e9199fc71b9157f2f59f5d67093bffb
Petr <petrlinkov1376@gmail.com>
2026-01-31 13:33:29 +0300

    app/models/products.py: Добавление связи с таблицей пользователей

--- app/models/products.py
+++ app/models/products.py
@@ -15,7 +15,12 @@ class Product(Base):
     stock: Mapped[int] = mapped_column(Integer)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
     category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"), nullable=False)
+    seller_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
     category: Mapped["Category"] = relationship(
         "Category",
         back_populates="product"
+    )
+    seller: Mapped["User"] = relationship(
+        "User",
+        back_populates="products"
     )
