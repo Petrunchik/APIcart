0c6cebd981b288413a674892b4a068dcf5e8566b
Petr <petrlinkov1376@gmail.com>
2026-02-06 14:18:37 +0300

    app/models/products.py: Добавлена новая связь и доработана модель данных

--- app/models/products.py
+++ app/models/products.py
@@ -12,8 +12,9 @@ class Product(Base):
     description: Mapped[str] = mapped_column(String(500), nullable=False)
     price: Mapped[Decimal] = mapped_column(Numeric(10, 2), nullable=False)
     image_url: Mapped[str | None] = mapped_column(String(200))
-    stock: Mapped[int] = mapped_column(Integer)
-    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    stock: Mapped[int] = mapped_column(Integer, default=0, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
+    rating: Mapped[Decimal] = mapped_column(Numeric(10, 2), default=0.0, nullable=False)
     category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"), nullable=False)
     seller_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
     category: Mapped["Category"] = relationship(
@@ -23,4 +24,8 @@ class Product(Base):
     seller: Mapped["User"] = relationship(
         "User",
         back_populates="products"
+    )
+    reviews: Mapped["Review"] = relationship(
+        "Review",
+        back_populates="product_review"
     )
