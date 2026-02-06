dc4320b69f1d8a7ad6e0a1bc33044f7e542a3d28
Petr <petrlinkov1376@gmail.com>
2026-02-06 14:17:31 +0300

    app/config.py: Добавление чувствительных файлов

--- app/config.py
+++ app/config.py
@@ -3,4 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = "HS256"
+DATABASE_URL = os.getenv("DATABASE_URL")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+REFRESH_TOKEN_EXPIRE_DAYS = os.getenv("REFRESH_TOKEN_EXPIRE_DAYS")
