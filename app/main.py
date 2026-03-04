9f83a2b3e8aacaf93cb7ef3f7199d002c8577487
Petr <petrlinkov1376@gmail.com>
2026-03-04 19:47:53 +0300

    Реализовано управление заказами

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routers import categories, products, users, reviews, cart
+from app.routers import categories, products, users, reviews, cart, orders
 
 app = FastAPI(
     title="APIcart",
@@ -11,3 +11,4 @@ app.include_router(products.router)
 app.include_router(users.router)
 app.include_router(reviews.router)
 app.include_router(cart.router)
+app.include_router(orders.router)
