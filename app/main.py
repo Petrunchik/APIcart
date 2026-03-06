0112f53376cf1005d8b1338e15aceb7c1d77e019
Petr <petrlinkov1376@gmail.com>
2026-03-06 10:47:48 +0300

    Добавление логирования

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from app.routers import categories, products, users, reviews, cart, orders
+from .log import log_middleware
 
 app = FastAPI(
     title="APIcart",
     version="0.0.1"
 )
+app.middleware("http")(log_middleware)
+
+
 app.mount("/media", StaticFiles(directory="media"), name="media")
 app.include_router(categories.router)
 app.include_router(products.router)
